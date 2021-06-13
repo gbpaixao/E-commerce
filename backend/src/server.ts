997fb38b5f5860eaa4177634dc87082a8d3f6fbf
gbpaixao <gustavobezerrapaixao@gmail.com>
2021-06-13 11:59:12 -0300

    git: backend initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const server = express();
+
+server.get("/", (request, response) => {
+  return response.send("e-commerce api");
+});
+
+server.listen(3030);
+
+export default server;
