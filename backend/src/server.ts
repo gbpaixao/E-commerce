cd156a96add0fddd39deb5ec22d085eed567bb84
gbpaixao <gustavobezerrapaixao@gmail.com>
2021-06-13 20:03:06 -0300

    feat: conexão com o banco de dados

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import routes from "./routes";
+import database from "./database"
 
 const server = express();
 
-server.get("/", (request, response) => {
-  return response.send("e-commerce api");
+server.get("/", async (req, res) => {
+  
+  try {
+    const answer = await database.select().from(`Usuario`);
+
+    if (answer.length === 0) return res.send({message: 'Não existem registros no BD.'})
+    return res.send({answer});
+} catch (error) {
+  console.log(`error: `, error)
+}
 });
 server.use(routes)
 
