15c6de880e595d1964499a60693e441f70d7aea8
gbpaixao <gustavobezerrapaixao@gmail.com>
2021-06-13 15:10:48 -0300

    feat: rotas de camisa, login e pedido criadas

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import routes from "./routes";
+
 const server = express();
 
 server.get("/", (request, response) => {
   return response.send("e-commerce api");
 });
+server.use(routes)
 
 server.listen(process.env.PORT || 3333);
 
