3afceb2f8ab885913b9389c474c334cbf3e0d8ef
Arthur Almeida <arthurdosantosalmeida@gmail.com>
2021-06-27 16:09:03 -0300

    feat: estrutura inicial para tela de home

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,9 +1,64 @@
-function Home(): JSX.Element {
+import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Button, Image } from 'react-bootstrap';
+import { Layout } from '../../components/Layout';
+
+import api from '../../services/api';
+
+import { ICamisa } from '../../types/Camisa';
+import { formatCurrency } from '../../utils/utils';
+
+export function Home(): JSX.Element {
+  const [camisas, setCamisas] = useState<ICamisa[]>([]);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    async function getCamisas() {
+      const response = await api.get('/camisas');
+
+      setCamisas(response.data.camisas);
+    }
+
+    getCamisas();
+  }, []);
+
   return (
-    <h1>
-      Homepage
-    </h1>
+    <Layout>
+      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '2rem' }}>
+        {camisas.map((camisa) => (
+          <div key={camisa.id}>
+            <div style={{ width: 256, height: 168 }}>
+              {/* <Image
+                src={camisa.mainPicture}
+                height={168}
+                width={256}
+                thumbnail
+              /> */}
+              <div style={{ height: 168, width: 256, background: 'red' }} />
+              <div style={{ padding: 25 }}>
+                <h5><b>{camisa.nomeCamisa}</b></h5>
+                <h5 style={{ marginBottom: 24 }}>{formatCurrency(camisa.valor)}</h5>
+
+                <div style={{
+                  display: 'flex',
+                  justifyContent: 'flex-end',
+                }}
+                >
+                  <Button
+                    type="button"
+                    onClick={() => history.push(`/descricaoCamisa/${camisa.id}`)}
+                    style={{ background: '#5227CC', border: 0 }}
+                  >
+                    Ver mais
+                  </Button>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        ))}
+      </div>
+    </Layout>
   );
 }
-
-export default Home;
