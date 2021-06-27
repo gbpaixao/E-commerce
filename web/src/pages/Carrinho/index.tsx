610c41db500f89d5158fa59c20490741f845a7c5
Arthur Almeida <arthurdosantosalmeida@gmail.com>
2021-06-27 18:56:06 -0300

    feat: lista todos os items adicionados ao carrinho

--- web/src/pages/Carrinho/index.tsx
+++ web/src/pages/Carrinho/index.tsx
@@ -1,46 +1,55 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Col, Image, Row } from 'react-bootstrap';
-import { Layout } from '../../components/Layout';
+import { FiTrash2 } from 'react-icons/fi';
 
-// import CamisaImg from '../../assets/camisa6.jpg';
+import { Layout } from '../../components/Layout';
 import ItemsAmount from '../../components/ItemsAmount';
+import { useCarrinho } from '../../contexts/CarrinhoContext';
+
+import { formatCurrency } from '../../utils/utils';
+import { styles } from './styles';
 
 export function Carrinho(): JSX.Element {
+  const { carrinho } = useCarrinho();
+
   return (
     <Layout>
-      <div className="d-flex">
-        <h3 style={{ marginLeft: 'calc(157px + 6rem + 15px)' }}>PRODUTO</h3>
-        <h3 style={{ marginLeft: '12.75rem' }}>QTD</h3>
-        <h3 style={{ marginLeft: '13rem' }}>SUBTOTAL</h3>
-      </div>
-
-      <div className="d-flex align-items-center">
-        <Image
-          src=""
-          height={157}
-          width={157}
-          style={{ marginRight: '6rem' }}
-          thumbnail
-        />
-
-        <Col style={{ marginRight: '6rem', minWidth: '15rem' }}>
-          <h4>Camisa do Flamengo</h4>
-          <h3>
-            <b>R$ 150,00</b>
-          </h3>
-        </Col>
-
-        <Col style={{ marginRight: '6rem' }}>
-          <ItemsAmount setCounter={() => {}} counter={0} />
-        </Col>
-
-        <Col>
-          <h4 style={{ minWidth: '10rem' }}>
-            <b>R$ 1500,00</b>
-          </h4>
-        </Col>
-
-        <Col />
+      <div>
+        <div className="d-flex">
+          <h3 style={{ marginLeft: 'calc(157px + 6rem + 15px)' }}>PRODUTO</h3>
+          <h3 style={{ marginLeft: '12.75rem' }}>QTD</h3>
+          <h3 style={{ marginLeft: '13.5rem' }}>SUBTOTAL</h3>
+        </div>
+        {carrinho.items?.map((item) => (
+          <div key={item.camisa.id} className="d-flex align-items-center">
+            <Image
+              src={item.camisa.mainPicture}
+              height={157}
+              width={157}
+              style={{ marginRight: '6rem' }}
+              thumbnail
+            />
+            <Col style={{ marginRight: '6rem', minWidth: '15rem' }}>
+              <h4>{item.camisa.nomeCamisa}</h4>
+              <h3>
+                <b>{formatCurrency(item.camisa.valor)}</b>
+              </h3>
+            </Col>
+            <Col style={{ marginRight: '6rem' }}>
+              <ItemsAmount setCounter={() => {}} counter={item.quantidade} />
+            </Col>
+            <Col>
+              <h4 style={{ minWidth: '10rem' }}>
+                <b>{formatCurrency(item.camisa.valor * item.quantidade)}</b>
+              </h4>
+            </Col>
+            <Col>
+              <button type="button" className={styles.trashButton}>
+                <FiTrash2 color="#5227CC" size={24} />
+              </button>
+            </Col>
+          </div>
+        ))}
       </div>
     </Layout>
   );
