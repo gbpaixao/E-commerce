4511990c9800bd0a9ac3045e6a86e4908e6197ad
Arthur Almeida <arthurdosantosalmeida@gmail.com>
2021-06-27 17:23:45 -0300

    refactor: usa o componente CardCamisa

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,17 +1,14 @@
 import { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { Button, Image } from 'react-bootstrap';
+import { CardCamisa } from '../../components/CardCamisa';
 import { Layout } from '../../components/Layout';
 
 import api from '../../services/api';
+import { styles } from './styles';
 
-import { ICamisa } from '../../types/Camisa';
-import { formatCurrency } from '../../utils/utils';
+import { Camisa } from '../../types/Camisa';
 
 export function Home(): JSX.Element {
-  const [camisas, setCamisas] = useState<ICamisa[]>([]);
-
-  const history = useHistory();
+  const [camisas, setCamisas] = useState<Camisa[]>([]);
 
   useEffect(() => {
     async function getCamisas() {
@@ -25,39 +22,8 @@ export function Home(): JSX.Element {
 
   return (
     <Layout>
-      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '2rem' }}>
-        {camisas.map((camisa) => (
-          <div key={camisa.id}>
-            <div style={{ width: 256, height: 168 }}>
-              {/* <Image
-                src={camisa.mainPicture}
-                height={168}
-                width={256}
-                thumbnail
-              /> */}
-              <div style={{ height: 168, width: 256, background: 'red' }} />
-              <div style={{ padding: 25 }}>
-                <h5><b>{camisa.nomeCamisa}</b></h5>
-                <h5 style={{ marginBottom: 24 }}>{formatCurrency(camisa.valor)}</h5>
-
-                <div style={{
-                  display: 'flex',
-                  justifyContent: 'flex-end',
-                }}
-                >
-                  <Button
-                    type="button"
-                    onClick={() => history.push(`/descricaoCamisa/${camisa.id}`)}
-                    style={{ background: '#5227CC', border: 0 }}
-                  >
-                    Ver mais
-                  </Button>
-                </div>
-              </div>
-
-            </div>
-          </div>
-        ))}
+      <div className={styles.container}>
+        {camisas.map((camisa) => <CardCamisa camisa={camisa} />)}
       </div>
     </Layout>
   );
