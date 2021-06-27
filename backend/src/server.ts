bfa469f63f20a95bd7ce778bbbbe5a5d6d66045a
gbpaixao <gustavobezerrapaixao@gmail.com>
2021-06-27 18:02:10 -0300

    feat(backend): fluxo de createCamisa criado

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,14 +3,7 @@ import routes from './routes/index.routes'
 
 const server = express()
 
-// import database from './database'
-// server.get('/', async (req, res) => {
-//   const data = await database.select().from('Usuario')
-
-//   if (data.length === 0)
-//     return res.json({ message: 'Não foram encontrados registros no BD.' })
-//   return res.json({ data })
-// })
+server.use(express.json())
 server.use(routes)
 
 server.listen(process.env.PORT || 3333, () => console.log("Api rodando."))
