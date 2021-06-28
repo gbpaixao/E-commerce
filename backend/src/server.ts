9b1de440b01e2af5fa53eb75111ecaf58368d575
gbpaixao <gustavobezerrapaixao@gmail.com>
2021-06-27 22:39:11 -0300

    feat: Middleware de captura de Errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import routes from './routes/index.routes'
 
 const server = express()
@@ -6,6 +6,19 @@ const server = express()
 server.use(express.json())
 server.use(routes)
 
-server.listen(process.env.PORT || 3333, () => console.log("Api rodando."))
+server.use((err: Error, request: Request, response: Response) => {
+  if (err instanceof Error) {
+    return response.status(400).json({
+      error: err.message,
+    })
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
+
+server.listen(process.env.PORT || 3333, () => console.log('Api rodando.'))
 
 export default server
