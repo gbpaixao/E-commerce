c620d22ca7da7373877b3bde0bcf20bbb039dc2b
gbpaixao <gustavobezerrapaixao@gmail.com>
2021-07-05 21:46:50 -0300

    feat: lógica de administrador para descrição de camisa

--- web/src/pages/Camisa/DescricaoCamisa/index.tsx
+++ web/src/pages/Camisa/DescricaoCamisa/index.tsx
@@ -29,12 +29,36 @@ export default function DescricaoCamisa(): JSX.Element {
   const admin = true;
 
   /* Handle Submit */
-  const handleSubmit = async () => {
+  const handleSubmitCart = async () => {
     try {
       setSubmitting(true);
       addItem(camisa.idCamisa, itemCarrinho);
 
       history.push('/carrinho');
+    } catch (error) {
+      toast.error('Houve algum problema!');
+    }
+    setSubmitting(false);
+  };
+
+  const handleSubmitUpdate = async () => {
+    try {
+      setSubmitting(true);
+
+      history.push(`/editarCamisa/${camisa.idCamisa}`);
+    } catch (error) {
+      toast.error('Houve algum problema!');
+    }
+    setSubmitting(false);
+  };
+
+  const handleSubmitDelete = async () => {
+    try {
+      setSubmitting(true);
+
+      await api.delete(`/camisas/${camisa.idCamisa}`);
+
+      history.push('/');
     } catch (error) {
       toast.error('Houve algum problema!');
     }
@@ -51,7 +75,6 @@ export default function DescricaoCamisa(): JSX.Element {
               <Image src={getRandomTshirt()} width={75} height={95} />
               <Image src={getRandomTshirt()} width={75} height={95} />
               <Image src={getRandomTshirt()} width={75} height={95} />
-              <Image src={getRandomTshirt()} width={75} height={95} />
             </div>
 
             <div>
@@ -60,78 +83,77 @@ export default function DescricaoCamisa(): JSX.Element {
           </div>
 
           <div className={styles.descriptionContainer}>
-            <h1>{camisa.nomeCamisa}</h1>
+            <h1>{camisa?.nomeCamisa}</h1>
 
-            <p>{camisa.descricao}</p>
+            <p>{camisa?.descricao}</p>
 
-            {/* { admin && ( */}
-            <Form.Group controlId="tamanho-camisa">
-              <Form.Label>
-                <b>Tamanho</b>
-              </Form.Label>
-              <br />
-              <Button
-                style={{
-                  backgroundColor: '#5227CC',
-                  borderColor: '#5227CC',
-                  boxShadow: 'none',
-                  width: '70px',
-                  cursor: 'default',
-                }}
-              >
-                {camisa.tamanho}
-              </Button>
-            </Form.Group>
-            {/* )} */}
-
-            {/* { admin && ( */}
-            <div className={styles.descriptionContainerInputGroup}>
-              <Form.Group controlId="nome-jogador" style={{ width: '200px' }}>
+            <div>
+              <Form.Group controlId="tamanho-camisa">
                 <Form.Label>
-                  <b>Nome Jogador</b>
+                  <b>Tamanho</b>
                 </Form.Label>
-                <Form.Control
-                  aria-label="nome-jogador"
-                  value={itemCarrinho.nomeJogador}
-                  onChange={(event) => setItemCarrinho({
-                    ...itemCarrinho,
-                    nomeJogador: event.target.value,
-                  })}
-                  maxLength={12}
-                />
-                <Form.Text>
-                  <small>Máximo de 12 caracteres</small>
-                </Form.Text>
-              </Form.Group>
-
-              <Form.Group controlId="numero-jogador" style={{ width: '126px' }}>
-                <Form.Label>
-                  <b>Nº do Jogador</b>
-                </Form.Label>
-                <Form.Control
-                  aria-label="numero-jogador"
-                  value={itemCarrinho.numeroJogador}
-                  onChange={(event) => {
-                    setItemCarrinho({
-                      ...itemCarrinho,
-                      numeroJogador: event.target.value,
-                    });
+                <br />
+                <Button
+                  style={{
+                    backgroundColor: '#5227CC',
+                    borderColor: '#5227CC',
+                    boxShadow: 'none',
+                    width: '70px',
+                    cursor: 'default',
                   }}
-                  maxLength={2}
-                />
-                <Form.Text>
-                  <small>Máximo de 2 caracteres</small>
-                </Form.Text>
+                >
+                  {camisa?.tamanho}
+                </Button>
               </Form.Group>
+
+              { !admin && (
+                <div className={styles.descriptionContainerInputGroup}>
+                  <Form.Group controlId="nome-jogador" style={{ width: '200px' }}>
+                    <Form.Label>
+                      <b>Nome Jogador</b>
+                    </Form.Label>
+                    <Form.Control
+                      aria-label="nome-jogador"
+                      value={itemCarrinho?.nomeJogador}
+                      onChange={(event) => setItemCarrinho({
+                        ...itemCarrinho,
+                        nomeJogador: event.target.value,
+                      })}
+                      maxLength={12}
+                    />
+                    <Form.Text>
+                      <small>Máximo de 12 caracteres</small>
+                    </Form.Text>
+                  </Form.Group>
+                  <Form.Group controlId="numero-jogador" style={{ width: '126px' }}>
+                    <Form.Label>
+                      <b>Nº do Jogador</b>
+                    </Form.Label>
+                    <Form.Control
+                      aria-label="numero-jogador"
+                      value={itemCarrinho?.numeroJogador}
+                      onChange={(event) => {
+                        setItemCarrinho({
+                          ...itemCarrinho,
+                          numeroJogador: event.target.value,
+                        });
+                      }}
+                      maxLength={2}
+                    />
+                    <Form.Text>
+                      <small>Máximo de 2 caracteres</small>
+                    </Form.Text>
+                  </Form.Group>
+                </div>
+              )}
             </div>
-            {/* )} */}
           </div>
 
           <div className={styles.shippingContainer}>
             <div>
-              <h1>{`R$ ${camisa.valor?.toFixed(2).replace('.', ',')}`}</h1>
+              <h1>{`R$ ${camisa?.valor?.toFixed(2).replace('.', ',')}`}</h1>
               <small>
-                {`6x de R$ ${(camisa.valor / 6)
+                {`6x de R$ ${(camisa?.valor / 6)
                   .toFixed(2)
                   .replace('.', ',')}`}
 
@@ -139,21 +161,33 @@ export default function DescricaoCamisa(): JSX.Element {
             </div>
 
             <div>
+              { !admin && (
+                <Form.Group controlId="quantidade-camisa">
+                  <Form.Label><b>Quantidade</b></Form.Label>
+                  <ItemsAmount
+                    counter={itemCarrinho?.quantidade}
+                    setCounter={(childData: number) => {
+                      setItemCarrinho({ ...itemCarrinho, quantidade: childData });
+                    }}
+                    estoque={camisa?.estoque}
+                  />
+                  <Form.Text>
+                    <small>{`${camisa?.estoque} unidades restantes`}</small>
+                  </Form.Text>
+                </Form.Group>
+              )}
+
+              { admin && (
               <Form.Group controlId="quantidade-camisa">
-                <Form.Label>Quantidade</Form.Label>
-                <ItemsAmount
-                  counter={itemCarrinho.quantidade}
-                  setCounter={(childData: number) => {
-                    setItemCarrinho({ ...itemCarrinho, quantidade: childData });
-                  }}
-                  estoque={camisa.estoque}
-                />
+                <Form.Label><b>Quantidade</b></Form.Label>
                 <Form.Text>
-                  <small>{`${camisa.estoque} unidades restantes`}</small>
+                  {`${camisa?.estoque} unidades restantes`}
                 </Form.Text>
               </Form.Group>
+              )}
             </div>
 
+            { !admin && (
             <div>
               <Form.Group controlId="frete">
                 <Form.Label>Calcular Frete</Form.Label>
@@ -165,11 +199,13 @@ export default function DescricaoCamisa(): JSX.Element {
                 </InputGroup>
               </Form.Group>
             </div>
+            )}
 
             <div className={styles.addToCart}>
+              { !admin && (
               <Button
                 type="submit"
-                onClick={handleSubmit}
+                onClick={handleSubmitCart}
                 style={{
                   backgroundColor: '#5227CC',
                   borderColor: '#5227CC',
@@ -197,6 +233,41 @@ export default function DescricaoCamisa(): JSX.Element {
                   'Adicionar ao carrinho'
                 )}
               </Button>
+              )}
+
+              { admin && (
+                <div style={{
+                  display: 'flex', justifyContent: 'space-evenly', flexDirection: 'column', height: 100,
+                }}
+                >
+                  <Button
+                    type="submit"
+                    onClick={handleSubmitUpdate}
+                    style={{
+                      backgroundColor: '#5227CC',
+                      borderColor: '#5227CC',
+                      width: '100%',
+                    }}
+                    disabled={isSubmitting}
+                  >
+                    Editar Camisa
+                  </Button>
+
+                  <Button
+                    type="submit"
+                    onClick={handleSubmitDelete}
+                    style={{
+                      backgroundColor: 'transparent',
+                      borderColor: '#495057',
+                      color: '#495057',
+                      width: '100%',
+                    }}
+                    disabled={isSubmitting}
+                  >
+                    Excluir Camisa
+                  </Button>
+                </div>
+              )}
             </div>
           </div>
         </div>
