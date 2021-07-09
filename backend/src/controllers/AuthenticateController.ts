1261306d319fb61c7d2ea7f208fa20c5982381fd
gbpaixao <gustavobezerrapaixao@gmail.com>
2021-07-09 18:03:18 -0300

    feat: Autenticação JWT

---
+++ backend/src/controllers/AuthenticateController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { AuthenticateService } from "../services/AuthenticateService";
+
+class AuthenticateController {
+
+ async handle(request: Request, response: Response) {
+   const {email, password} = request.body
+
+  const authenticateService = new AuthenticateService()
+
+  const token = await authenticateService.execute({email, password})
+
+  return response.json({token})
+ }
+}
+
+export {AuthenticateController}
