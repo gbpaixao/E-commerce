64ee247ed79c36a697114d4e39e456824ebfb3b9
MarceloSilv4 <50931232+MarceloSilv4@users.noreply.github.com>
2021-07-12 19:48:25 -0300

    feat: tela de acompanhar pedido pronta, falta deixar funcional

--- web/src/pages/AcompanharPedido/index.tsx
+++ web/src/pages/AcompanharPedido/index.tsx
@@ -1,75 +1,237 @@
 import {
-    Alert, Col, Image, Row,
-  } from 'react-bootstrap';
-  import { FiTrash2 } from 'react-icons/fi';
-  
-  import { Layout } from '../../components/Layout';
-  import ItemsAmount from '../../components/ItemsAmount';
-  import { useCarrinho } from '../../contexts/CarrinhoContext';
-  
-  import { formatCurrency } from '../../utils/utils';
-  import { styles } from './styles';
-  import { getRandomTshirt } from '../../server/getRandomTshirt';
-  
-  export function AcompanharPedido(): JSX.Element {
-    const { carrinho, removeItem, updateItemAmount } = useCarrinho();
-  
-    console.log(carrinho);
-  
-    return (
-      <Layout>
-        {carrinho.items.length ? (
+  Alert, Col, Image, Row, Button,
+} from 'react-bootstrap';
+import { FiHome } from 'react-icons/fi';
+import { ImHome } from 'react-icons/im';
+import { RiStore3Fill } from 'react-icons/ri';
+import { MdLocalShipping } from 'react-icons/md';
+
+import { Layout } from '../../components/Layout';
+import ItemsAmount from '../../components/ItemsAmount';
+import { useCarrinho } from '../../contexts/CarrinhoContext';
+
+import { formatCurrency } from '../../utils/utils';
+import { styles } from './styles';
+import { getRandomTshirt } from '../../server/getRandomTshirt';
+
+export function AcompanharPedido(): JSX.Element {
+  return (
+    <Layout>
+
+      <div>
+        <div className="d-flex align-items-center" style={{ background: 'rgba(222, 226, 230, 1)', borderRadius: '10px' }}>
+          <Image
+            src={getRandomTshirt()}
+            height={140}
+            width={104}
+            thumbnail
+            style={{ marginLeft: '5rem', marginTop: '0.5rem', marginBottom: '0.5rem' }}
+          />
+          <Col>
+            <p>N°</p>
+            <b>43612</b>
+          </Col>
+          <Col style={{ minWidth: '15rem' }}>
+            <p>Previsão de entrega</p>
+            <b>29/08/2021</b>
+          </Col>
+          <Col>
+            <p>Total</p>
+            <b>R$100,00</b>
+          </Col>
+          <Col style={{
+            minWidth: '10rem',
+            marginRight: '5rem',
+            marginLeft: '2rem',
+          }}
+          >
+            <Button
+              variant="light"
+              size="sm"
+            >
+              Cancelar pedido
+            </Button>
+          </Col>
+        </div>
+
+        <div
+          className="d-flex align-items-center"
+          style={{
+            marginTop: '2rem',
+            marginLeft: '5rem',
+            marginRight: '5rem',
+          }}
+        >
+
           <div>
-            <div className="d-flex">
-              <h3 style={{ marginLeft: 'calc(157px + 6rem + 15px)' }}>CAMISA</h3>
-              <h3 style={{ marginLeft: '12.75rem' }}>QTD</h3>
-              <h3 style={{ marginLeft: '13.5rem' }}>SUBTOTAL</h3>
+            <div
+              style={{
+                width: '5rem',
+                height: '5rem',
+                background: 'rgba(222, 226, 230, 1)',
+                borderRadius: '100px',
+                alignItems: 'center',
+                justifyContent: 'center',
+                display: 'flex',
+              }}
+            >
+              <RiStore3Fill color="#5227CC" size={30} />
             </div>
-            {carrinho.items?.map((item) => (
-              <div key={item.camisa.idCamisa} className="d-flex align-items-center">
-                <Image
-                  src={getRandomTshirt()}
-                  height={157}
-                  width={157}
-                  style={{ marginRight: '6rem' }}
-                  thumbnail
-                />
-                <Col style={{ marginRight: '6rem', minWidth: '15rem' }}>
-                  <h4>{item.camisa.nomeCamisa}</h4>
-                  <h3>
-                    <b>{formatCurrency(item.camisa.valor)}</b>
-                  </h3>
-                </Col>
-                <Col style={{ marginRight: '6rem' }}>
-                  <ItemsAmount
-                    setCounter={(quantidade: number) => (
-                      updateItemAmount(quantidade, item.camisa.idCamisa))}
-                    counter={item.quantidade}
-                  />
-                </Col>
-                <Col>
-                  <h4 style={{ minWidth: '10rem' }}>
-                    <b>{formatCurrency(item.camisa.valor * item.quantidade)}</b>
-                  </h4>
-                </Col>
-                <Col>
-                  <button
-                    type="button"
-                    className={styles.trashButton}
-                    onClick={() => removeItem(item.camisa.idCamisa)}
-                  >
-                    <FiTrash2 color="#5227CC" size={24} />
-                  </button>
-                </Col>
-              </div>
-            ))}
+            <div
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                display: 'flex',
+              }}
+            >
+              <p> Loja </p>
+            </div>
           </div>
-        ) : (
-          <Alert variant="secondary">
-            Seu carrinho está vazio
-          </Alert>
-        )}
-      </Layout>
-    );
-  }
-  
+
+          <hr style={{
+            minWidth: '33%',
+          }}
+          />
+
+          <div>
+            <div style={{
+              width: '5rem',
+              height: '5rem',
+              background: 'rgba(222, 226, 230, 1)',
+              borderRadius: '100px',
+              alignItems: 'center',
+              justifyContent: 'center',
+              display: 'flex',
+            }}
+            >
+              <MdLocalShipping color="#5227CC" size={30} />
+            </div>
+            <div
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                display: 'flex',
+              }}
+            >
+              <p> Correios </p>
+            </div>
+          </div>
+
+          <hr style={{
+            minWidth: '33%',
+          }}
+          />
+
+          <div>
+            <div style={{
+              width: '5rem',
+              height: '5rem',
+              background: 'rgba(222, 226, 230, 1)',
+              borderRadius: '100px',
+              alignItems: 'center',
+              justifyContent: 'center',
+              display: 'flex',
+            }}
+            >
+              <ImHome color="#5227CC" size={30} />
+            </div>
+            <div
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                display: 'flex',
+              }}
+            >
+              <p> Entregue </p>
+            </div>
+          </div>
+        </div>
+
+        <div
+          style={{
+            justifyContent: 'space-between',
+            display: 'flex',
+            marginRight: '5rem',
+            marginLeft: '5rem',
+            marginTop: '2rem',
+          }}
+        >
+
+          <div
+            style={{
+              alignItems: 'space-between',
+              justifyContent: 'space-between',
+              display: 'flex',
+            }}
+          >
+            <div
+              style={{
+                marginLeft: '1rem',
+                width: '3rem',
+              }}
+            >
+              <p>Atualização</p>
+              <p>Atualização</p>
+              <p>Atualização</p>
+              <p>Atualização massas</p>
+
+            </div>
+
+            <div
+              style={{
+                marginLeft: '5rem',
+                width: '2rem',
+              }}
+            >
+              <p>Status</p>
+            </div>
+
+          </div>
+
+          <div
+            style={{
+              background: 'rgba(222, 226, 230, 1)',
+              width: '20rem',
+              borderRadius: '10px',
+            }}
+          >
+            <div
+              style={{
+                background: 'rgba(222, 226, 230, 1)',
+                marginLeft: '1rem',
+                marginTop: '1rem',
+              }}
+            >
+              <h5> Código de rastreio</h5>
+              <p>HFAAF</p>
+            </div>
+
+            <div
+              style={{
+                background: 'rgba(222, 226, 230, 1)',
+                marginLeft: '1rem',
+                marginTop: '1rem',
+              }}
+            >
+              <h5> Data de compra </h5>
+              <p>HFAAF</p>
+            </div>
+
+            <div
+              style={{
+                background: 'rgba(222, 226, 230, 1)',
+                marginLeft: '1rem',
+                marginTop: '1rem',
+              }}
+            >
+              <h5> Previsão de entrega </h5>
+              <p>HFAAF</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </Layout>
+
+  );
+}
--- web/src/pages/AcompanharPedido/styles.tsx
+++ web/src/pages/AcompanharPedido/styles.tsx
@@ -1,8 +1,22 @@
 import { css } from '@emotion/css';
 
 export const styles = {
-  trashButton: css`
-    background: transparent;
+  cancelButton: css`
+    background: white;
     border: none;
-  `,
+    margin: 4px 2px;
+    text-align: center;
+    alignItems: center;
+    width: 100;
+    display: flex;
+      `,
+  radioDiv: css`
+  width: '6rem';
+  height: '6rem';
+  background: 'rgba(222; 226; 230; 1)';
+  borderRadius: '100px';
+  alignItems: 'center';
+  justifyContent: 'center';
+  display: 'flex';
+    `,
 };
