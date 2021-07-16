57db261ee46a690e580d8dcb3d4ff43fcd5edc80
gbpaixao <gustavobezerrapaixao@gmail.com>
2021-07-15 23:35:44 -0300

    feat: informações do usuário em /auth

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -1,21 +1,21 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import loginRoutes from "./login.routes";
-import camisaRoutes from "./camisa.routes";
-import pedidoRoutes from "./pedido.routes";
-import { AuthenticateController } from "../controllers/AuthenticateController";
-import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import loginRoutes from './login.routes';
+import camisaRoutes from './camisa.routes';
+import pedidoRoutes from './pedido.routes';
+import { AuthenticateController } from '../controllers/AuthenticateController';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
-const routes = Router()
+const routes = Router();
 
-const authenticateController = new AuthenticateController()
+const authenticateController = new AuthenticateController();
 
-routes.get('/', (req, res) => res.send("api rodando"))
-routes.post('/auth', authenticateController.handle)
+routes.get('/', (req, res) => res.send('api rodando'));
+routes.post('/auth', authenticateController.handle);
 
-routes.use(ensureAuthenticated)
-routes.use(loginRoutes)
-routes.use(camisaRoutes)
-routes.use(pedidoRoutes)
+routes.use(ensureAuthenticated);
+// routes.use(loginRoutes)
+routes.use(camisaRoutes);
+routes.use(pedidoRoutes);
 
-export default routes
+export default routes;
